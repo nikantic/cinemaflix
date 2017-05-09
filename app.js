35e6854cb476ec85cb4957d576236809c3eb0476
Nikola Antic <nikantic@outlook.com>
2017-05-09 18:07:29 +0000

    initial commit

---
+++ app.js
@@ -0,0 +1,26 @@
+var express = require("express"),
+    app     = express(),
+    request = require("request");
+
+app.set("view engine", "ejs");
+app.use(express.static(__dirname + "/public"));
+
+// ROUTES
+app.get("/", function(req, res){
+    res.render("home");
+});
+
+app.get("/results", function(req, res){
+    var query = req.query.search;
+    var url = "http://www.omdbapi.com/?s=" + query;
+    request(url, function(error, response, body){
+        if(!error && response.statusCode == 200) {
+            var data = JSON.parse(body);
+            res.render("results", {data: data, queryInfo: query});
+        }
+    });
+});
+
+app.listen(process.env.PORT, process.env.IP, function(){
+    console.log("Server has started!");     
+});
